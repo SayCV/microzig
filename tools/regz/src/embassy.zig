a0d98e7bd12726a6d077f31df64aa0d9e59dc802
SayCV <sssxyc@163.com>
2026-01-14 16:11:21 +0800

    skip access null value for parsed stm32-data-generated

--- tools/regz/src/embassy.zig
+++ tools/regz/src/embassy.zig
@@ -323,6 +323,11 @@ pub fn load_into_db(io: std.Io, db: *Database, path: []const u8) !void {
                 if (item.object.get("fieldset")) |fieldset| blk: {
                     const fieldset_key = try std.fmt.allocPrint(allocator, "fieldset/{s}", .{fieldset.string});
                     const fieldset_value = (register_file.value.object.get(fieldset_key) orelse break :blk).object;
+                    const fields = fieldset_value.get("fields");
+                    if (fields == null) {
+                        std.log.err("No fields found for {s}", .{fieldset.string});
+                        return;
+                    }
                     next_field: for (fieldset_value.get("fields").?.array.items) |field| {
                         const field_name = field.object.get("name").?.string;
                         const field_description: ?[]const u8 = if (field.object.get("description")) |desc| desc.string else null;
@@ -531,6 +536,11 @@ fn resolve_inheritance_recursively(allocator: std.mem.Allocator, json_data: *std
 
         //Get access to the parent and its list of items.
         const parent = try get_parent(allocator, json_data, child_full_name, parent_unqualified_name.string);
+        const object_list_name = parent.value_ptr.object.get(list_name);
+        if (object_list_name == null) {
+            std.log.err("No list_name({s}) found for parent_unqualified_name({s})", .{ list_name, parent_unqualified_name.string });
+            return;
+        }
         const parent_section_array = parent.value_ptr.object.get(list_name).?.array;
 
         // If our dictionary doesn't contain an item present in the child add it to the list
