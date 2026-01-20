9e45e16b0cb0841623ab4cfbb7f32edab9f85a64
SayCV <sssxyc@163.com>
2026-01-20 12:29:29 +0800

    port/hsc/hsc32 - initial

--- port/hsc/hsc32/src/hal.zig
+++ port/hsc/hsc32/src/hal.zig
@@ -1,11 +1,8 @@
 const microzig = @import("microzig");
 
-pub const compatibility = @import("hal/compatibility.zig");
 pub const clocks = @import("hal/clocks.zig");
 pub const gpio = @import("hal/gpio.zig");
 pub const i2c = @import("hal/i2c.zig");
-pub const i2cdma = @import("hal/i2cdma.zig");
-pub const spim = @import("hal/spim.zig");
 pub const time = @import("hal/time.zig");
 pub const uart = @import("hal/uart.zig");
 pub const drivers = @import("hal/drivers.zig");
@@ -24,8 +21,6 @@ test "hal tests" {
     _ = clocks;
     _ = gpio;
     _ = i2c;
-    _ = i2cdma;
-    _ = spim;
     _ = time;
     _ = uart;
 
